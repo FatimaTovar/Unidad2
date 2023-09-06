378fd92296054253ddc83aff583d09744e2e7ea2
FatimaTovar <143622018+FatimaTovar@users.noreply.github.com>
2023-09-06 08:39:26 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listatareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "Lavar los trastes";
+        String tarea2 = " Barrer";
+        String tarea3 = " Hacer la comida";
+        String tarea4 = " Lavar la ropa";
+        String tarea5 = " Limpiar mi cuarto";
+        
+        String cadenaTexto = "Cadena de texto !!! %% &&&";
+        
+        System.out.println("Mi tarea es " + tarea1 + tarea2 + tarea3 + tarea4 + tarea5);
+    }
+    
+}
